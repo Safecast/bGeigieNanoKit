484d3be23641c306a20e3b589a3e9854a63e1b74
robouden <rob@yr-design.biz>
2013-05-06 19:34:22 +0900

    V1.2.2
    Fixed xGeigie display mode

--- bGeigieNano.ino
+++ bGeigieNano.ino
@@ -913,7 +913,7 @@ bool gps_gen_timestamp(TinyGPS &gps, char *buf, unsigned long counts, unsigned l
     uphour = uptime/3600;
     upminute = uptime/60 - uphour*60;
     sprintf_P(strbuffer, PSTR("%02dh%02dm"), uphour, upminute);
-    display.setCursor(92, offset+24);
+    display.setCursor(92, offset+16);
     display.setTextSize(1);
     display.setTextColor(WHITE);
     display.println(strbuffer);
@@ -993,7 +993,7 @@ bool gps_gen_timestamp(TinyGPS &gps, char *buf, unsigned long counts, unsigned l
     if (toggle) {
       // Display distance
       dtostrf((float)(gps_distance/1000.0), 0, 1, strbuffer);
-      display.setCursor(116-(strlen(strbuffer)*6), offset+16); // textsize*8
+      display.setCursor(116-(strlen(strbuffer)*6), offset+24); // textsize*8
       display.print(strbuffer);
       sprintf_P(strbuffer, PSTR("km"));
       display.println(strbuffer);
@@ -1004,7 +1004,7 @@ bool gps_gen_timestamp(TinyGPS &gps, char *buf, unsigned long counts, unsigned l
       } else {
         sprintf_P(strbuffer, PSTR("--"));
       }
-      display.setCursor(122-(strlen(strbuffer)*6), offset+16); // textsize*8
+      display.setCursor(122-(strlen(strbuffer)*6), offset+24); // textsize*8
       display.print(strbuffer);
       display.println("m");
     }
@@ -1158,9 +1158,15 @@ bool gps_gen_timestamp(TinyGPS &gps, char *buf, unsigned long counts, unsigned l
   int battery =((read_voltage(VOLTAGE_PIN)-30));
   if (battery < 0) battery = 0;	
   if (battery > 8) battery = 8;
-
-display.drawRect(116, offset+0, 12, 7, WHITE);
+  
+if (config.type == GEIGIE_TYPE_X){
+display.drawRect(116, offset+24, 12, 7, WHITE);
+display.fillRect(118, offset+26, battery, 3, WHITE);
+ } else {
+  display.drawRect(116, offset+0, 12, 7, WHITE);
 display.fillRect(118, offset+2, battery, 3, WHITE);
+}
+
   
   display.display();
 #endif
