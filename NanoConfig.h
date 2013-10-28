388758ae78281d0116917c193b057e5921345dd1
Lionel Bergeret <lbergeret@gmail.com>
2013-10-28 09:26:49 +0900

    V1.3.0

--- NanoConfig.h
+++ NanoConfig.h
@@ -6,7 +6,7 @@
 //
 
 #define NANO_DEVICE_ID        0000
-#define NANO_VERSION       "1.2.9"
+#define NANO_VERSION       "1.3.0"
 #define NANO_HEADER        "BNRDD"
 #define NANO_CPM_FACTOR        334
 #define NANO_BQM2_FACTOR        37
